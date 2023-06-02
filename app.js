24cf54b25bdc045a8ce7053ac07fcb9d317954ef
IMiroI <givaudan.matthieu@gmail.com>
2023-06-02 10:43:26 +0200

    Project Initilization

---
+++ app.js
@@ -0,0 +1,19 @@
+
+// Requiring module
+const express = require('express');
+ 
+// Creating express object
+const app = express();
+ 
+// Handling GET request
+app.get('/', (req, res) => {
+    res.send('Ici le Début de la fin')
+    res.end()
+})
+ 
+// Port Number
+const PORT = process.env.PORT || 5000;
+ 
+// Server Setup
+app.listen(PORT,console.log(
+  `Server started on port ${PORT}`));
